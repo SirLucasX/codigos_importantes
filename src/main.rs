35a77ed9968c70f18925e8865ded927828af00e2
Lucas Daniel <manolc14@gmail.com>
2022-09-16 15:46:08 -0300

    Codigo mais importante adicionado

---
+++ src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let mut numero: u8 = 0;
+
+    while numero <= 100{
+
+        if numero % 2 == 0{
+            println!("{numero}");
+        }
+        
+        numero+=1;
+    }
+}
